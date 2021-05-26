d8605d32b364c28f71075f74cdd55d6458a1734d
Tomoloju Victor <tomoloju45@gmail.com>
2021-05-26 07:10:57 -1100

    welcome screen updates

--- ScreenTask.cpp
+++ ScreenTask.cpp
@@ -1,4 +1,5 @@
 #include "ScreenTask.h"
+#include "customImages.h"
 
 using namespace ScreenCtrl;
 
@@ -10,10 +11,11 @@ void ScreenTask::IntScreen(){
 
 void ScreenTask::WelcomeMessage(){
 
-    tft.setCursor(100, 110);
-    tft.setTextColor(TFT_WHITE);  tft.setTextSize(2);
-    tft.println("Hello World!");
-    delay(1500);
-    PowerMode(0);
-    tft.fillScreen(TFT_BLACK);
+    // tft.setCursor(100, 110); 78 80 99
+    // tft.setTextColor(TFT_WHITE);  tft.setTextSize(2);
+tft.drawBitmap(10, 90, plogo, 60, 52, TFT_SPECIAL);
+tft.drawBitmap(75, 90, flowlogo, 235, 99, TFT_LIGHTGREY);
+// delay(1500);
+// PowerMode(0);
+// tft.fillScreen(TFT_BLACK);
 }
